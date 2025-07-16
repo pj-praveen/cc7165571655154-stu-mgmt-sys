d33145ea1db5596d2d81863c1a0998c9185de33e
pj-praveen <pj.praveenkumar143@gmail.com>
2025-07-16 08:28:06 +0530

    commit message

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold mb-4 text-center">Student Management System</h1>
+      <Home />
+    </>
+  )
+}
+
+export default App
